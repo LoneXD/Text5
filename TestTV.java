87443849d5a827aabf257e6a74d4d7857850844a
LoneXD <91311157+LoneXD@users.noreply.github.com>
2022-01-02 22:13:26 +0800

    Add files via upload

---
+++ TestTV.java
@@ -0,0 +1,24 @@
+package text5;
+
+public class TestTV {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		TV tv1 = new TV();
+		tv1.turnOn();
+		tv1.setChannel(30);
+		tv1.setVolume(3);
+		
+		TV tv2 = new TV();
+		tv2.turnOn();
+		tv2.channelUp();
+		tv2.channelUp();
+		tv2.volumeUp();
+		
+		System.out.println("tv1's channel is " + tv1.channel
+				+ " and volume leve is "+ tv1.volumeLevel);
+		System.out.println("tv2's channel is " + tv2.channel
+				+ " and volume leve is "+ tv2.volumeLevel);
+	}
+
+}
